0803d836373c012f6194d136ab35004b75e82710
Althaf-official <althaf.jereh@gmail.com>
2023-10-13 05:51:42 +0400

    added static root and added storages on installed app

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -166,4 +167,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
