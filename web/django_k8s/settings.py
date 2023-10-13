675c5e42001a65727606bd76caf830b0b1654d5e
Althaf-official <althaf.jereh@gmail.com>
2023-10-13 23:46:52 +0400

    make test on locally -(venv) althaf@mas:~/dev/django-k8s$ cd web/ (venv) althaf@mas:~/dev/django-k8s/web$ ls collectstatic.sh  entrypoint.sh  requirements.txt db.sqlite3        manage.py      staticfiles django_k8s        migrate.sh     staticfiles-cdn Dockerfile        posts (venv) althaf@mas:~/dev/django-k8s/web$ python manage.py test {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'dockerdc', 'USER': 'myuser', 'PASSWORD': 'mysecretpassword', 'HOST': 'localhost', 'PORT': '5434'}} Creating test database for alias 'default'... System check identified no issues (0 silenced). F ====================================================================== FAIL: test_failure (posts.tests.PostTestCase.test_failure) ---------------------------------------------------------------------- Traceback (most recent call last):   File "/home/althaf/dev/django-k8s/web/posts/tests.py", line 7, in test_failure     self.assertTrue(False) AssertionError: False is not true
    ----------------------------------------------------------------------
    Ran 1 test in 0.005s
    
    FAILED (failures=1)
    Destroying test database for alias 'default'...
    (venv) althaf@mas:~/dev/django-k8s/web$ python manage.py makemigrations
    {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'dockerdc', 'USER': 'myuser', 'PASSWORD': 'mysecretpassword', 'HOST': 'localhost', 'PORT': '5434'}}
    No changes detected
    (venv) althaf@mas:~/dev/django-k8s/web$ python manage.py migrate
    {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'dockerdc', 'USER': 'myuser', 'PASSWORD': 'mysecretpassword', 'HOST': 'localhost', 'PORT': '5434'}}
    Operations to perform:
      Apply all migrations: admin, auth, contenttypes, sessions
    Running migrations:
      No migrations to apply.
    (venv) althaf@mas:~/dev/django-k8s/web$ python manage.py makemigrations
    {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'dockerdc', 'USER': 'myuser', 'PASSWORD': 'mysecretpassword', 'HOST': 'localhost', 'PORT': '5434'}}
    Migrations for 'posts':
      posts/migrations/0001_initial.py
        - Create model Post
    (venv) althaf@mas:~/dev/django-k8s/web$ python manage.py migrate
    {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'dockerdc', 'USER': 'myuser', 'PASSWORD': 'mysecretpassword', 'HOST': 'localhost', 'PORT': '5434'}}
    Operations to perform:
      Apply all migrations: admin, auth, contenttypes, posts, sessions
    Running migrations:
      Applying posts.0001_initial... OK
    (venv) althaf@mas:~/dev/django-k8s/web$ python manage.py test
    {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': 'dockerdc', 'USER': 'myuser', 'PASSWORD': 'mysecretpassword', 'HOST': 'localhost', 'PORT': '5434'}}
    Creating test database for alias 'default'...
    System check identified no issues (0 silenced).
    F
    ======================================================================
    FAIL: test_failure (posts.tests.PostTestCase.test_failure)
    ----------------------------------------------------------------------
    Traceback (most recent call last):
      File "/home/althaf/dev/django-k8s/web/posts/tests.py", line 7, in test_failure
        self.assertTrue(False)
    AssertionError: False is not true
    
    ----------------------------------------------------------------------
    Ran 1 test in 0.003s
    
    FAILED (failures=1)
    Destroying test database for alias 'default'...

--- web/django_k8s/settings.py
+++ web/django_k8s/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #external
     'storages',
+    #internal
+    'posts',
 ]
 
 MIDDLEWARE = [
